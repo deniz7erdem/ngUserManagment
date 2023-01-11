906d58928265c2ad953197399a1e4630dc42f869
Deniz Erdem <47831143+deniz7erdem@users.noreply.github.com>
2023-01-11 16:21:49 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -7,9 +7,9 @@ CREATE TABLE public.users (
 );
 
 
-INSERT INTO public.users (userid, username, usernick, userpassword, useradmin) VALUES (1, 'Deniz Erdem', 'admin', '$2b$10$.eT9UB8ThfKAC.dcpO85OeOa7ltb3MpWLl.Qa58m33idJCWxZll4O', '1');
-INSERT INTO public.users (userid, username, usernick, userpassword, useradmin) VALUES (3, 'Deniz Erdem', 'deniz7erdem', '$2b$10$4UfN2vjdrtfv42yFFm6BnuoURzALdz6lf1JvSX2I.tggwo39reAwS', '0');
-INSERT INTO public.users (userid, username, usernick, userpassword, useradmin) VALUES (2, 'Ahmet Can', 'acanh', '$2b$10$P5.AIG0Maq4RiRgvFw3qCe6pS2pQPoeU8vKMabsADY6aOuY7bDVFK', '0');
-INSERT INTO public.users (userid, username, usernick, userpassword, useradmin) VALUES (4, 'Buse Ç', 'admin2', '$2b$10$8PrMf/mBUlF/yJNesrhgluL/2/OYVyHSXJV4ivBySQm8MsG5gr53O', '1');
-INSERT INTO public.users (userid, username, usernick, userpassword, useradmin) VALUES (5, 'Tuna Han', 'tuna35', '$2b$10$YC.58cLHDK7cwMkX.pwdvOd8BO9j9ynuy6RhfrzCCfb/MmwemFuEO', '0');
+INSERT INTO public.users ( username, usernick, userpassword, useradmin) VALUES ( 'Deniz Erdem', 'admin', '$2b$10$.eT9UB8ThfKAC.dcpO85OeOa7ltb3MpWLl.Qa58m33idJCWxZll4O', '1');
+INSERT INTO public.users ( username, usernick, userpassword, useradmin) VALUES ('Deniz Erdem', 'deniz7erdem', '$2b$10$4UfN2vjdrtfv42yFFm6BnuoURzALdz6lf1JvSX2I.tggwo39reAwS', '0');
+INSERT INTO public.users ( username, usernick, userpassword, useradmin) VALUES ('Ahmet Can', 'acanh', '$2b$10$P5.AIG0Maq4RiRgvFw3qCe6pS2pQPoeU8vKMabsADY6aOuY7bDVFK', '0');
+INSERT INTO public.users ( username, usernick, userpassword, useradmin) VALUES ( 'Buse Ç', 'admin2', '$2b$10$8PrMf/mBUlF/yJNesrhgluL/2/OYVyHSXJV4ivBySQm8MsG5gr53O', '1');
+INSERT INTO public.users ( username, usernick, userpassword, useradmin) VALUES ( 'Tuna Han', 'tuna35', '$2b$10$YC.58cLHDK7cwMkX.pwdvOd8BO9j9ynuy6RhfrzCCfb/MmwemFuEO', '0');
 
